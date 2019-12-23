03a4b7bb1af278f4fab7791c0f2350add7aa8949
zhangliezhuo <576750937@qq.com>
2019-12-23 17:47:20 +0800

    add experiment train

--- train.py
+++ train.py
@@ -1,21 +1,17 @@
 import argparse
 import json
-
+from pathlib import Path
 import dataloader as D
 import models as M
 from utils import loss as L
 
 from trainer import Trainer
 
-def main():
-	parser = argparse.ArgumentParser(description='Semantic Segmentation for Training...')
 
-	parser.add_argument('-c', '--config', default='./configs/visiontek_road_rgb_unet_res101.json', 
-		type=str, help='Path to the config file.')
-
-	args = parser.parse_args()
+def train(config_path):
+	print(f"Start training from config file {config_path}.")
 	# load configs from .json
-	configs = json.load(open(args.config))
+	configs = json.load(open(config_path))
 	# initial train and valid loader
 	train_loader = getattr(D, configs["loader_name"])(configs["train_loader"])
 	val_loader = getattr(D, configs["loader_name"])(configs["val_loader"])
@@ -31,6 +27,23 @@ def main():
 		)
 	# start training
 	trainer.train()
+
+def main():
+	parser = argparse.ArgumentParser(description='Semantic Segmentation for Training...')
+
+	parser.add_argument('-c', '--config', default='./configs/visiontek_road_rgb_unet_res101.json', 
+		type=str, help='Path to the config file.')
+	parser.add_argument('-c_dir', "--config_dir", default=None,
+		type=str, help="Dir Path to the config files.")
+
+	args = parser.parse_args()
+	if args.config_dir:
+		config_dir = Path(args.config_dir)
+		config_paths = config_dir.glob("*.json")
+		for config in config_paths:
+			train(config)
+	else:
+		train(args.config)
 
 if __name__ == '__main__':
 	main()
